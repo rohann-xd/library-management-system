df6491ca5e16f981e43ca7c9dd6689b3df1bc5dd
Rohan <rohansadhwani5@gmail.com>
2024-12-10 13:50:36 +0530

    Custom User Model Setup

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from authentication.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+class UserModelAdmin(BaseUserAdmin):
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["id", "email", "first_name","last_name", "is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["first_name", "last_name"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "first_name", "last_name", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email", "id"]
+    filter_horizontal = []
+
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserModelAdmin)
--- configuration/settings.py
+++ configuration/settings.py
@@ -134,6 +134,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'authentication.User'
+
 
 # Django project settings.py
 SIMPLE_JWT = {
