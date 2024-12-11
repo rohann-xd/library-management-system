68722f992156363b4a94e8a8cc20de63f2b78d64
Rohan <rohansadhwani5@gmail.com>
2024-12-11 11:36:15 +0530

    Initial Database Schema designed

--- librarian/models.py
+++ librarian/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=13, unique=True)
+    copies = models.IntegerField(default=1)
+
+class BorrowRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    request_date = models.DateField(auto_now_add=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=10, choices=[
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Denied', 'Denied')
+    ])
+
+class BorrowHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField()
+    return_date = models.DateField()
